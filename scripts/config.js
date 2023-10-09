bac10aa0a38179de758f4af7982240a072015b76
JussyJ <48039566+JussyJ@users.noreply.github.com>
2023-10-09 15:17:37 +0800

    Add files via upload

---
+++ scripts/config.js
@@ -0,0 +1,18 @@
+export default {
+    // Token Implicit Grant Client ID
+    // clientID: '2bbc162b-326f-4a7d-83ac-651fde859923',
+    clientID: '360513bd-fe43-46e7-bb79-b7fb3ae38f62',
+
+    redirectUri: 'https://localhost/',
+
+    genesysCloud: {
+        // Genesys Cloud region
+        // eg. 'mypurecloud.ie', 'euw2.pure.cloud', etc...
+        // region: 'mypurecloud.com'
+        // region: 'mypurecloud.com'
+        region: 'usw2.pure.cloud'
+    },
+
+    translateServiceURI: 'https://localhost:8443/translate'
+    // translateServiceURI: 'https://localhost/'
+}
